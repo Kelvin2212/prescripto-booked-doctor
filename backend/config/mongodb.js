50c6cf1ad2b978da3dc5f0dea5cebb54b573213d
Kelvin_Samuel <kelvinbalkes@gmail.com>
2024-10-22 10:26:31 +0700

    add configure to deploy

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import moongose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("DB connected"));
-  await moongose.connect(`${process.env.MONGODB_URI}/prescripto`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
 };
 
 export default connectDB;
